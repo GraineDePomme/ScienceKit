42c44be82debff2ce26a9ad1bc76bfddc2fac978
GraineDePomme <Guillaume.Sarisson@gmail.com>
2025-09-26 17:38:40 +0200

    Fixing rounding functions.

--- Sources/Math/math.swift
+++ Sources/Math/math.swift
@@ -3,21 +3,21 @@ import Numerics
 
 public extension Double {
     func rounded(toPlaces places: Int) -> Double {
-        let multiplier: Double = .pow(10.0, Double(places))
+        let multiplier: Double = Double.pow(10.0, Double(places))
         return (self * multiplier).rounded() / multiplier
     }
 }
 
 public extension Float32 {
     func rounded(toPlaces places: Int) -> Float32 {
-        let multiplier: Float32 = .pow(10.0, Float32(places))
+        let multiplier: Float32 = Float32.pow(10.0, Float32(places))
         return (self * multiplier).rounded() / multiplier
     }
 }
 
 public extension Float16 {
     func rounded(toPlaces places: Int) -> Float16 {
-        let multiplier: Float16 = .pow(10.0, Float16(places))
+        let multiplier: Float16 = Float16.pow(10.0, Float16(places))
         return (self * multiplier).rounded() / multiplier
     }
 }
