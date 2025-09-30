1d9f0ca73f113f88d8ffb43e0dc06e0511602e36
GraineDePomme <Guillaume.Sarisson@gmail.com>
2025-09-30 18:44:36 +0200

    Adding a few modules

--- Sources/Math/math.swift
+++ Sources/Math/math.swift
@@ -3,21 +3,36 @@ import Numerics
 
 public extension Double {
     func rounded(toPlaces places: Int) -> Double {
-        let multiplier: Double = Double.pow(10.0, Double(places))
-        return (self * multiplier).rounded() / multiplier
+        guard self != 0 else { return 0 }
+    
+        let d: Double = ceil(Double.log10(abs(self)))
+        let power: Int = places - Int(d)
+        let magnitude: Double = Double.pow(10.0, Double(power))
+    
+        return (self * magnitude).rounded() / magnitude
     }
 }
 
 public extension Float32 {
     func rounded(toPlaces places: Int) -> Float32 {
-        let multiplier: Float32 = Float32.pow(10.0, Float32(places))
-        return (self * multiplier).rounded() / multiplier
+        guard self != 0 else { return 0 }
+    
+        let d: Float32 = ceil(Float32.log10(abs(self)))
+        let power: Int = places - Int(d)
+        let magnitude: Float32 = Float32.pow(10.0, Float32(power))
+    
+        return (self * magnitude).rounded() / magnitude
     }
 }
 
 public extension Float16 {
     func rounded(toPlaces places: Int) -> Float16 {
-        let multiplier: Float16 = Float16.pow(10.0, Float16(places))
-        return (self * multiplier).rounded() / multiplier
+        guard self != 0 else { return 0 }
+    
+        let d: Float16 = ceil(Float16.log10(abs(self)))
+        let power: Int = places - Int(d)
+        let magnitude: Float16 = Float16.pow(10.0, Float16(power))
+    
+        return (self * magnitude).rounded() / magnitude
     }
 }
