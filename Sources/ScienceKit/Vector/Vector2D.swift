5d4a394e8babb7ecf43a17ee8381102c21cb60ae
Guillaume Sarisson <Guillaume.Sarisson@gmail.com>
2025-10-25 12:06:18 +0200

    Fixing vector 2D

--- Sources/ScienceKit/Vector/Vector2D.swift
+++ Sources/ScienceKit/Vector/Vector2D.swift
@@ -6,18 +6,18 @@ public struct vector2D {
     public var y: Double
 
     public var r: Double
-    public var θ: Double
+    public var theta: Double
 
     public init(x: Double, y: Double) {
         self.x = x
         self.y = y
         self.r = sqrt(x*x + y*y)
-        self.θ = atan2(self.y, self.x)
+        self.theta = atan2(self.y, self.x)
     }
 
     public init(r: Double, theta: Double) {
         self.r = r
-        self.θ = theta
+        self.theta = theta
         self.x = r * cos(theta)
         self.y = r * sin(theta)
     }
