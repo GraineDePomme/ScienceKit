73a451c335fdf6c87d075692adb6fbf9b605b67d
Guillaume Sarisson <Guillaume.Sarisson@gmail.com>
2025-10-25 18:46:25 +0200

    Adding some constants

---
+++ Sources/ScienceKit/Constants/MathConstant.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public extension Double {
+
+    /// The tau constant, or 2 * π
+    static let tau: Double = 2 * Double.pi
+
+}
--- Sources/ScienceKit/Constants/PhysicalConstants.swift
+++ Sources/ScienceKit/Constants/PhysicalConstants.swift
@@ -1,20 +1,29 @@
 import Foundation
 
-extension Double {
+public extension Double {
 
     /// The standard acceleration of gravitation : m . s^-2
-    public static let standardGravityAcceleration: Double = 9.806_65
+    static let standardGravityAcceleration: Double = 9.806_65
 
     /// The Newtonian constant of gravitation : m^3 kg^-1 s^-2
-    public static let newtonianGravitationalConstant: Double = 6.674_30e-11
+    static let newtonianGravitationalConstant: Double = 6.674_30e-11
 
     /// The mass of the Earth : kg
-    public static let earthMass: Double = 5.9736e24
+    static let earthMass: Double = 5.9736e24
 
     /// The volumetric mean radius of the Earth : m
-    public static let earthRadius: Double = 6371e3
+    static let earthRadius: Double = 6371e3
 
-    // The speed of light in vacuum : m s^-1
-    public static let speedOfLight: Double = 299_792_458
+    /// The speed of light in vacuum : m s^-1
+    static let speedOfLight: Double = 299_792_458
+
+    /// The Planck constant : H Hz^-1
+    static let planckConstant: Double = 6.626_070_15e-34
+
+    /// The Planck constant over 2π : J s
+    static let reducedPlanckConstant: Double = planckConstant / Double.tau
+
+    /// The mass of an electron : kg
+    static let electronMass: Double = 9.109_383_7139e-31
 
 }
