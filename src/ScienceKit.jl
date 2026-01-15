d13fa1f5aa7d9110549639397ad21e4d4461463c
GraineDePomme <Guillaume.Sarisson@gmail.com>
2026-01-15 12:40:44 +0100

    Hello, World!

---
+++ src/ScienceKit.jl
@@ -0,0 +1,8 @@
+module ScienceKit
+
+include("constants.jl")
+
+using .Constants
+export Constants
+
+end # module ScienceKit
---
+++ src/constants.jl
@@ -0,0 +1,7 @@
+module Constants
+
+export rydberg_constant
+
+const rydberg_constant::Float64 = 10_973_731.568_157
+
+end
