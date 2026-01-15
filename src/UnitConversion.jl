83b86ee43fde407e7f502d1060a471697bc9c4cf
GraineDePomme <Guillaume.Sarisson@gmail.com>
2026-01-15 13:00:50 +0100

    Adding some length conversion

--- src/UnitConversion.jl
+++ src/UnitConversion.jl
@@ -7,4 +7,27 @@ export Length
     Nanometers
 end
 
+function convert(value::Float64, from::Length, exponent::Float64, to::Length)
+    base_unit = from
+    target_unit = to
+
+    result = value
+
+    # First we convert the value in meters ...
+    if base_unit == Meters
+        result = result * 1
+    elseif base_unit == Nanometers
+        result = result * 10e-9 ^ exponent
+    end
+
+    # ... then we convert it in the unit we really want
+    if base_unit == Meters
+        result = result / 1
+    elseif base_unit == Nanometers
+        result = result / 10e-9 ^ exponent
+    end
+
+    return result
+end
+
 end
